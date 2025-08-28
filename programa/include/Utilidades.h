c8f4977d2438cbba92a2101b6b2bc45584a30ce7
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-08-28 12:28:56 -0600

    Update utilidades

---
+++ programa/include/Utilidades.h
@@ -0,0 +1,3 @@
+/*Este archivo contiene funciones utilitarias para el programa, se refiere a funciones que a lo largo
+del proyecto se van a reutilizar en diferentes partes del código, tambien validaciones de entrada y
+salida, manejo de errores, entre otros. */
